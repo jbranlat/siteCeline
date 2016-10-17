ea649e3028ccb2de27b0669714c181013662ab19
Clément Ibanez <clement.ibanez@etu.u-bordeaux.fr>
2016-10-17 22:54:04 +0200

    Uniformisation du menu et de l'entête

---
+++ js/onLoad.js
@@ -0,0 +1,5 @@
+$(function() {
+	//Chargement à la page du menu et de l'entête
+	$("#business").append('<div class="brand">Céline Vallet - Ostéopathe D.O.</div><div class="address-bar">25-27 rue Gérard Blot | 33400 Talence | 06.52.66.35.91</div>');
+	$("#menu").append('<nav class="navbar navbar-default" role="navigation"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Business Casual</a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"><ul class="nav navbar-nav"><li><a href="index.html">Accueil</a></li><li><a href="about.html">L\'osthépathie</a></li><li><a href="blog.html">Infos Pratiques</a></li><li><a href="contact.html">Contact</a></li></ul></div></div></nav>');
+});
